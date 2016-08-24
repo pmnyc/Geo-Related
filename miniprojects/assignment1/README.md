e44f673d57fe9a2c6f4be2b85cd25eb2c2bba1d3
pm <pmiori@gmail.com>
2016-08-24 08:10:56 -0400

    rm

--- miniprojects/assignment1/README.md
+++
@@ -1,3 +0,0 @@
-# Questions
-
-## add
--- miniprojects/assignment1/__init__.py
+++
@@ -1,9 +0,0 @@
-import fellow
-import typecheck
-
-@fellow.app.task(name="assignment1.add")
-@typecheck.test_cases(x=[1, 2], y=[5, 6.0])
-@typecheck.returns("number")
-def add(x, y):
-    """ change code here to return the sum of x and y """
-    return x+y
